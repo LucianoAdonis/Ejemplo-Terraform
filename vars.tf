7c01097d20cf685ba3d5480b0a0c23c2903e1102
Luciano Adonis <luciano.adonisv@gmail.com>
2017-10-06 17:19:44 -0300

    Create vars.tf

---
+++ vars.tf
@@ -0,0 +1,39 @@
+variable "AWS_ACCESS_KEY" {
+	default = "<VAR>"
+}
+variable "AWS_SECRET_KEY" {
+	default = "<VAR>"
+}
+variable "AWS_REGION" {
+  default = "<VAR>"
+}
+variable "AMIS" {
+  type = "map"
+  default = {
+    us-east-1 = "<VAR>"
+  }
+}
+
+variable "PATH_TO_SECURITY" {
+  default = "<VAR>"
+}
+
+variable "PATH_TO_PRIVATE_IP" {
+  default = "<VAR>"
+}
+
+variable "PATH_TO_ROLE" {
+  default = "<VAR>"
+}
+
+variable "PATH_TO_AWS_KEY_NAME}" {
+  default = "<VAR>"
+}
+
+variable "PATH_TO_INSTANCE_TYPE}" {
+  default = "<VAR>"
+}
+
+variable "INSTANCE_USERNAME" {
+  default = "ubuntu"
+}
